9a2b9ce32a184b2b581d1361b47dc6cb9a6ee51a
TomasCv0628 <tomas2224508@correo.uis.edu.co>
2022-10-27 06:33:40 -0500

    Dearrollo programa. PlanTelefonia. TomasCv0628

--- README.md
+++ README.md
@@ -2,4 +2,11 @@ Realice el análisis(identificación de clases, atributos,métodos), diseño(Dia
 
 Crear un programa para gestionar un plan de telefonía móvil. EL plan tiene un numero de celular, un operador, una cantidad de minutos y un costo por minuto, Calcular el total a pagar teniendo en cuenta que si el operador es “movilujo” tiene un 50% de descuento. Usar mínimo dos métodos constructores.
 
+![Ejecucion1](eje1.png)
+
+![Ejecucion2](eje2.png)
+
+![Ejecucion3](eje3.png)
+
+![DiagramaClases](diagramaClases.png)
 
--- modelo/PlanTelefonia.java
+++ modelo/PlanTelefonia.java
@@ -6,19 +6,27 @@ public class PlanTelefonia
     private int cantMin;
     private double precioMin;
     private double precioPlan;
+    private int numero;
 
 
-    public PlanTelefonia(String pOperador, int pCantMin, double pPrecioMin)
+    public PlanTelefonia(String pOperador, int pCantMin, double pPrecioMin, int pNum)
     {
         operador = pOperador;
         cantMin = pCantMin;
         precioMin = pPrecioMin;
+        numero = pNum;
     }
-
+    public PlanTelefonia()
+    {
+        operador = "";
+        cantMin = 0;
+        precioMin = 0;
+        numero = 0;
+    }
     public void calcularPlan()
     {
         double precio = 0;
-        if (operador.equals("movilujo"))
+        if (operador.equals("MOVILUJO"))
         {
             precio = (cantMin * precioMin)*0.5;
         }
@@ -32,4 +40,22 @@ public class PlanTelefonia
     {
         return precioPlan;
     }
+
+    public void setCantMin(int pCantMin)
+    {
+        this.cantMin = pCantMin;
+    }
+    public void setCostMin(double pCostMin)
+    {
+        this.precioMin = pCostMin;
+    }
+    public void setOpe(String pOpe)
+    {
+        this.operador = pOpe;
+    }
+    public void setTel(int pNum)
+    {
+        this.numero = pNum;
+    }
+    
 }
